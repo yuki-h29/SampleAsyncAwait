15850f725e45fda75107181ec3f12adf0267faf5
yuki-h29 <yuki.hirano29@gmail.com>
2023-08-06 22:39:18 +0900

    Initial Commit

---
+++ SampleAsyncAwait/SampleAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAsyncAwaitApp.swift
+//  SampleAsyncAwait
+//
+//  Created by 平野裕貴 on 2023/08/06.
+//
+
+import SwiftUI
+
+@main
+struct SampleAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
